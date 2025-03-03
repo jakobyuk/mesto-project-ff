9ceeb916d3fd80826dfc6f9e2edcfb62b72ba8d9
jakobyuk <dashayava.666@gmail.com>
2025-03-04 00:19:39 +0500

    iteration 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
 
-// @todo: DOM узлы
+function deleteCard(event) {
+  const card = event.target.closest('.card');
+  card.remove();
+}
 
-// @todo: Функция создания карточки
+function addCard(cardContent) {
+  const cardElement = cardTemplate.content.cloneNode(true);
 
-// @todo: Функция удаления карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);;
 
-// @todo: Вывести карточки на страницу
+  cardImage.src = cardContent.link;
+  cardImage.alt = cardContent.name;
+  cardTitle.textContent = cardContent.name;
+
+  return cardElement;
+}
+
+const placesList = document.querySelector('.places__list');
+
+function renderCard(cardContent) {
+  const cardElement = addCard(cardContent);
+  placesList.appendChild(cardElement);
+  }
+  
+initialCards.forEach(renderCard);
